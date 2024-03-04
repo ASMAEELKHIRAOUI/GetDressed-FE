a26ded766d172c1cdaab090a388f3c3634a8085f
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2024-03-04 18:01:20 +0100

    working on displaying products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'], 
 })
 export class AppComponent {
-  
+
    title = "get dressed"
    constructor(private http: HttpClient, private modalService: NgbModal) {}
 
